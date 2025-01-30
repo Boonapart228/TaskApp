85a85319ba191f7ef0140c9e8d6262504b09316c
Boonapart <kolyha101@gmail.com>
2025-01-30 15:14:39 +0200

    Fixed text to textId and improved visuals

--- app/src/main/java/com/example/taskapp/presentation/bottom_bar/model/BottomBarItems.kt
+++ app/src/main/java/com/example/taskapp/presentation/bottom_bar/model/BottomBarItems.kt
@@ -2,12 +2,16 @@ package com.example.taskapp.presentation.bottom_bar.model
 
 import com.example.taskapp.R
 
-enum class BottomBarItems(val route: String, val icon: Int, val text: String) {
-    HOME(route = "Home_Screen", icon = R.drawable.home, text = "Home"),
+enum class BottomBarItems(
+    val route: String = "",
+    val icon: Int = R.drawable.home,
+    val textId: Int = R.string.empty_text
+) {
+    HOME(route = "Home_Screen", icon = R.drawable.home, textId = R.string.home_bottom_bar_text),
     CATEGORIES(
         route = "Categories_Screen",
         icon = R.drawable.list,
-        text = "Categories"
+        textId = R.string.categories_bottom_bar_text
     ),
-    DEFAULT("", R.drawable.home, "")
+    DEFAULT
 }
