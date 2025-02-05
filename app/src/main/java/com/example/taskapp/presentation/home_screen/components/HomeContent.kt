35dc1aa6bf1900b533875ae8761d2a9f8fcefa13
Boonapart <kolyha101@gmail.com>
2025-02-05 12:34:35 +0200

    Implement home screen ui

--- app/src/main/java/com/example/taskapp/presentation/home_screen/components/HomeContent.kt
+++ app/src/main/java/com/example/taskapp/presentation/home_screen/components/HomeContent.kt
@@ -1,14 +1,28 @@
 package com.example.taskapp.presentation.home_screen.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskapp.R
 import com.example.taskapp.presentation.bottom_bar.BottomBar
+import com.example.taskapp.presentation.home_screen.contents.HomeListBar
 import com.example.taskapp.presentation.home_screen.contents.HomeTopBar
 import com.example.taskapp.presentation.navigation.model.Screens
+import com.example.taskapp.ui.theme.LocalDimen
 
 
 @Composable
@@ -22,11 +36,46 @@ fun HomeContent(
                 onClick = onBottomBarNavigationClick,
                 selectedNavigate = state.selectedScreen
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                shape = CircleShape
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add, contentDescription = null,
+                    modifier = Modifier.size(LocalDimen.current.homeIconFABSize)
+                )
+            }
         }) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "Home")
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(top = LocalDimen.current.homeColumnPaddingTop)
+        ) {
+            HomeListBar(textId = R.string.pinned_notes_text)
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(horizontal = LocalDimen.current.verticalGridHorizontalPadding),
+                modifier = Modifier.height(LocalDimen.current.lazyVerticalGridHeight)
+            ) {
+
+            }
+            Spacer(modifier = Modifier.padding(top = LocalDimen.current.homeSpacerPaddingTop))
+            HomeListBar(textId = R.string.list_notes_text)
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(horizontal = LocalDimen.current.verticalGridHorizontalPadding)
+            ) {
+            }
+
         }
 
     }
+}
 
+@Composable
+@Preview
+fun HomeContentPreview() {
+    HomeContent({}, HomeState())
 }
---
+++ app/src/main/java/com/example/taskapp/presentation/home_screen/contents/HomeTaskDetails.kt
@@ -0,0 +1,100 @@
+package com.example.taskapp.presentation.home_screen.contents
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskapp.R
+import com.example.taskapp.ui.theme.LocalDimen
+
+
+@Composable
+fun HomeTaskDetails(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = Modifier.padding(
+            horizontal = LocalDimen.current.homeDetailsCardHorizontalPadding,
+            vertical = LocalDimen.current.homeDetailsCardVerticalPadding
+        ),
+        shape = RoundedCornerShape(LocalDimen.current.categoriesCardShape)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(LocalDimen.current.categoriesColumnSpaceBy),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(LocalDimen.current.categoriesColumnPaddingAll)
+        ) {
+            Text(
+                text = title, fontSize = LocalDimen.current.categoriesTextSize,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = description,
+                    fontSize = LocalDimen.current.categoriesTextSizeSmall,
+                    fontWeight = FontWeight.Normal,
+                    maxLines = 3,
+                    minLines = 3,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = LocalDimen.current.categoriesTextBottomPadding)
+                )
+            }
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.BottomEnd) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(LocalDimen.current.homeDetailsHorizontalSpacedBy),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.time_ui),
+                        contentDescription = null,
+                        Modifier.size(LocalDimen.current.homeDetailsIconSize)
+                    )
+                    Text(
+                        text = "09:22", textAlign = TextAlign.Center,
+                        fontSize = LocalDimen.current.homeDetailsDataTextSize
+                    )
+                    Text(text = "|", textAlign = TextAlign.Center)
+                    Icon(
+                        imageVector = Icons.Default.DateRange, contentDescription = null,
+                        Modifier.size(LocalDimen.current.homeDetailsIconSize)
+                    )
+                    Text(text = "2023/07/01", textAlign = TextAlign.Center, fontSize = LocalDimen.current.homeDetailsDataTextSize)
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+@Preview
+fun HomeTaskDetailsPreview() {
+    HomeTaskDetails(
+        "Title",
+        "You said you'll be with me forever\\nWith you personally, I am absolutely sincere."
+    )
+}
